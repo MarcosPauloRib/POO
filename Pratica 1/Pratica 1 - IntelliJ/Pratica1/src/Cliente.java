c5a3317a45d60b7d2b90da7cce78bbcf9760d6a4
MarcosPauloRib <142666108+MarcosPauloRib@users.noreply.github.com>
2023-09-04 17:26:26 -0300

    Delete Pratica 1 directory

--- Pratica 1/Pratica 1 - IntelliJ/Pratica1/src/Cliente.java
+++
@@ -1,7 +0,0 @@
-public class Cliente {
-    private String nome;
-    private int cpf;
-    private int cep;
-    private String endereco;
-
-}
--- Pratica 1/Pratica 1 - IntelliJ/Pratica1/src/Transacao.java
+++
@@ -1,7 +0,0 @@
-public class Transacao {
-    private int credit;
-    private int debit;
-    private int boleto;
-    private int pix;
-
-}
